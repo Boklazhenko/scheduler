5f2f30062a41dfccea211031e91b6d94d2842874
boklazhenko <aboklazhenko@stream.ru>
2020-12-15 13:39:54 +0500

    multiple db

--- scheduler.go
+++ scheduler.go
@@ -3,107 +3,53 @@ package scheduler
 import (
 	"context"
 	"github.com/emirpasic/gods/sets/treeset"
+	"sync"
 	"sync/atomic"
 	"time"
 )
 
-const chanBuffSize = 1000
+const chanBuffSize = 10000
+const implCount = 10
 const inactivityInterval = time.Hour
 
 type Job struct {
-	t        int64
-	i        time.Duration
-	j        func()
-	s        *Scheduler
+	time     int64
+	interval time.Duration
+	job      func()
 	every    bool
 	canceled int32
 }
 
 func compare(lhs, rhs interface{}) int {
-	return int(lhs.(*Job).t - rhs.(*Job).t)
+	return int(lhs.(*Job).time - rhs.(*Job).time)
 }
 
 func (j *Job) Cancel() {
 	atomic.StoreInt32(&j.canceled, 1)
-	j.s.removeCh <- j
 }
 
 type Scheduler struct {
-	set      *treeset.Set
-	removeCh chan *Job
 	insertCh chan *Job
-	timer    *time.Timer
 }
 
 func New() *Scheduler {
 	return &Scheduler{
-		set:      treeset.NewWith(compare),
-		removeCh: make(chan *Job, chanBuffSize),
 		insertCh: make(chan *Job, chanBuffSize),
 	}
 }
 
 func (s *Scheduler) Run(ctx context.Context) {
-	s.timer = time.NewTimer(inactivityInterval)
-	for {
-		select {
-		case now := <-s.timer.C:
-			if s.set.Empty() {
-				s.timer.Reset(inactivityInterval)
-			} else {
-				i := s.set.Iterator()
-				i.First()
-				j := i.Value().(*Job)
+	wg := sync.WaitGroup{}
 
-				if atomic.LoadInt32(&j.canceled) == 0 {
-					go j.j()
-				}
-
-				s.set.Remove(j)
-				if j.every {
-					j.t = now.UnixNano() + j.i.Nanoseconds()
-					s.insertCh <- j
-				}
-				i = s.set.Iterator()
-				if i.First() {
-					s.timer.Reset(time.Duration(i.Value().(*Job).t - now.UnixNano()))
-				} else {
-					s.timer.Reset(inactivityInterval)
-				}
-			}
-		case j := <-s.removeCh:
-			i := s.set.Iterator()
-			if i.First() {
-				if i.Value().(*Job).t == j.t {
-					if !s.timer.Stop() {
-						<-s.timer.C
-					}
-					if i.Next() {
-						s.timer.Reset(time.Duration(i.Value().(*Job).t - time.Now().UnixNano()))
-					} else {
-						s.timer.Reset(inactivityInterval)
-					}
-				}
-				s.set.Remove(j)
-			}
-		case j := <-s.insertCh:
-			for ; s.set.Contains(j); j.t++ {
-			}
-			i := s.set.Iterator()
-			if !i.First() || j.t < i.Value().(*Job).t {
-				if !s.timer.Stop() {
-					<-s.timer.C
-				}
-				s.timer.Reset(time.Duration(j.t - time.Now().UnixNano()))
-			}
-			s.set.Add(j)
-		case <-ctx.Done():
-			if !s.timer.Stop() {
-				<-s.timer.C
-			}
-			return
-		}
+	for i := 0; i < implCount; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			newImpl(s).run(ctx)
+		}()
 	}
+
+	wg.Wait()
 }
 
 func (s *Scheduler) Once(interval time.Duration, j func()) *Job {
@@ -116,13 +62,73 @@ func (s *Scheduler) Every(interval time.Duration, j func()) *Job {
 
 func (s *Scheduler) insertJob(interval time.Duration, j func(), every bool) *Job {
 	job := &Job{
-		t:        time.Now().UnixNano() + interval.Nanoseconds(),
-		i:        interval,
-		j:        j,
-		s:        s,
+		time:     time.Now().UnixNano() + interval.Nanoseconds(),
+		interval: interval,
+		job:      j,
 		every:    every,
 		canceled: 0,
 	}
 	s.insertCh <- job
 	return job
+}
+
+type impl struct {
+	s *Scheduler
+}
+
+func newImpl(s *Scheduler) *impl {
+	return &impl{
+		s: s,
+	}
+}
+
+func (impl *impl) run(ctx context.Context) {
+	set := treeset.NewWith(compare)
+	timer := time.NewTimer(inactivityInterval)
+	for {
+		select {
+		case now := <-timer.C:
+			if set.Empty() {
+				timer.Reset(inactivityInterval)
+			} else {
+				i := set.Iterator()
+				i.First()
+				j := i.Value().(*Job)
+
+				set.Remove(j)
+
+				if atomic.LoadInt32(&j.canceled) == 0 {
+					go j.job()
+
+					if j.every {
+						j.time = now.UnixNano() + j.interval.Nanoseconds()
+						impl.s.insertCh <- j
+					}
+				}
+
+				i = set.Iterator()
+				if i.First() {
+					timer.Reset(time.Duration(i.Value().(*Job).time - now.UnixNano()))
+				} else {
+					timer.Reset(inactivityInterval)
+				}
+			}
+		case j := <-impl.s.insertCh:
+			for ; set.Contains(j); j.time++ {
+			}
+			i := set.Iterator()
+			if !i.First() || j.time < i.Value().(*Job).time {
+				if !timer.Stop() {
+					<-timer.C
+				}
+				timer.Reset(time.Duration(j.time - time.Now().UnixNano()))
+			}
+			set.Add(j)
+		case <-ctx.Done():
+			if !timer.Stop() {
+				<-timer.C
+			}
+			return
+		}
+	}
 }
